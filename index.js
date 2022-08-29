40266699986f458c12e3619ee6a15cae1ccfe648
PatrickARatcliff <pratcliff5@gmail.com>
2022-08-29 17:51:03 -0600

    add basic write function, create init for app, and init

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ const fs = require('fs');
 const Manager = require('Manager');
 const Engineer = require('Engineer');
 const Intern = require('Intern');
+
+//Create a function to write html file
+function writeToFile(fileName) {
+    inquirer.prompt(questions)
+       
+    .then((data) => {
+            fs.writeFile(fileName, genMD(data), (err) =>
+
+                err ? console.error(err) : console.log('An HTML file has been created for your project!')
+            )
+        }
+        );
+};
+//Create a function to initialize app
+function init() {
+    writeToFile('index.html');
+};
+//Function call to initialize app
+init();
