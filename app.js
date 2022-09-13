72990a158ef2268f738055ed9f88ac6d9fba09d6
PatrickARatcliff <pratcliff5@gmail.com>
2022-09-12 18:32:36 -0600

    debug htmlgenerateTeamHtml fuction

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Manager = require('./lib/Manager');
-const {managerQuestions} = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
-const {engineerQuestions} = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const {internQuestions} = require('./lib/Intern');
+const { Console } = require('console');
 //initialize team member array
 let team = [];
 //create html cards for different sub-classes of employees
-const managerCard = (teamMember) => { return `<div class="card" style="width: 18rem;">
+const managerCard = (teamMember) => {
+    return `<div class="card" style="width: 18rem;">
 <div class="card-header">
     <h3><i class="bi bi-cup-hot-fill"></i> Manager</h3>
     <h5>${teamMember.name}</h5>
@@ -17,11 +16,13 @@ const managerCard = (teamMember) => { return `<div class="card" style="width: 18
 <ul class="list-group list-group-flush">
   <li class="list-group-item">ID#: ${teamMember.id}</li>
   <li class="list-group-item">Email: ${teamMember.email}</li>
-  <li class="list-group-item">Office#: ${teamMember.office}</li>
+  <li class="list-group-item">Office#: ${teamMember.officeNumber}</li>
 </ul>
 </div>
-`; }
-const engineerCard = (teamMember) => { return `<div class="card" style="width: 18rem;">
+`;
+}
+const engineerCard = (teamMember) => {
+    return `<div class="card" style="width: 18rem;">
 <div class="card-header">
     <h3><i class="bi bi-wrench-adjustable-circle-fill"></i> Engineer</h3>
     <h5>${teamMember.name}</h5>
@@ -29,11 +30,13 @@ const engineerCard = (teamMember) => { return `<div class="card" style="width: 1
 <ul class="list-group list-group-flush">
   <li class="list-group-item">ID: ${teamMember.id}</li>
   <li class="list-group-item">Email: ${teamMember.email}</li>
-  <li class="list-group-item">Github Username: ${teamMember.username}</li>
+  <li class="list-group-item">Github Username: ${teamMember.gitHub}</li>
 </ul>
 </div>
-`; }
-const internCard = (teamMember) => { return`<div class="card" style="width: 18rem;">
+`;
+}
+const internCard = (teamMember) => {
+    return `<div class="card" style="width: 18rem;">
 <div class="card-header">
     <h3><i class="bi bi-mortarboard-fill"></i> Intern</h3>
     <h5>${teamMember.name}</h5>
@@ -44,17 +47,21 @@ const internCard = (teamMember) => { return`<div class="card" style="width: 18re
   <li class="list-group-item">School: ${teamMember.school}</li>
 </ul>
 </div>
-`; }
+`;
+}
 //create template literal html code based on members in team array
-generateTeamPageHtml = (team) => {
-    let buildTeam;
-    let teamHtml = buildTeam.join('');
+generateTeamPageHtml = () => {
+    let buildTeam = [];
+    //console.log(`team: ${team}`);
     for (let i = 0; i < team.length; i++) {
         const members = team[i];
+        console.log(`role: ${members.getRole()}`);
         members.getRole() === "manager" ? buildTeam.push(managerCard(members)) :
             members.getRole() === "engineer" ? buildTeam.push(engineerCard(members)) :
-                members.getRole() === "intern" ? buildTeam.push(internCard(members)) : console.log("No team members");
+                members.getRole() === "intern" ? buildTeam.push(internCard(members)) :
+                    console.log("No team members");
     }
+    console.log(buildTeam);
     return `<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -69,14 +76,49 @@ generateTeamPageHtml = (team) => {
         <header class="container">
             <h1>My Team</h1>
         </header>
-        <main class="container">
-            ${teamHtml}
-        </main>
-        
+        <main class="container d-inline-flex">
+            ${buildTeam}       
+        </main>  
     </body>
     </html>`;
 }
-//creat question wwith otions to create additional team members or finish page
+//inquirer questions for manager
+managerQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: "Enter name:",
+            name: 'name',
+            //validate: (answer) => {answer ? true : console.log("Enter a name"), false},
+        },
+        {
+            type: 'number',
+            message: "Enter employee identification number:",
+            name: 'id',
+            //validate: idInput => { idInput ? true : console.log("Enter an id number"), false || typeof idInput === 'number' ? true : console.log("id must be a number"), false }
+        },
+        {
+            type: 'input',
+            message: "Enter email address:",
+            name: 'email',
+            //validate: emailInput => { emailInput ? true : console.log("Enter an email address"), false || emailInput === 'anystring@anystring.anystring' ? true : console.log("Enter a valid email address"), false }
+        },
+        {
+            type: 'input',
+            message: "Enter the manager's office number:",
+            name: 'office',
+            //validate: officeInput => { officeInput ? true : console.log("Enter an office number"), false || typeof officeInput === 'number' ? true : console.log("Office number must be an interger"), false }
+        },
+    ])
+        .then(answers => {
+            const manager = new Manager(answers.name, answers.id, answers.email, answers.office);
+            //push new manager to team array
+            console.log(manager);
+            team.push(manager);
+            choiceQuestion();
+        })
+};
+//create question with options to create additional team members or finish page
 const choiceQuestion = () => {
     return inquirer.prompt([
         {
@@ -87,35 +129,98 @@ const choiceQuestion = () => {
         }
     ])
         .then(choice => {
-            switch (choice) {
+            console.log(choice.choice)
+            switch (choice.choice) {
                 case "Add an Engineer":
                     //inquirer questions for engineer
                     engineerQuestions();
-                     //loop back through options
-                    choiceQuestion();
                     break;
                 case "Add an Intern":
                     //inquirer questions for intern
                     internQuestions();
-                    //loop back through options
-                    choiceQuestion();
                     break;
                 default:
-                    //create html for team page
-                    generateTeamPageHtml();
+                    //create html for team page  
+                    generateTeamPageHtml(team);
+                    init();
                     break;
             }
         });
 }
+//inquirer questions for engineer
+engineerQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: "Enter name:",
+            name: 'name',
+            //validate: nameInput => { nameInput ? true : console.log("Enter a name"), false }
+        },
+        {
+            type: 'number',
+            message: "Enter employee identification number:",
+            name: 'id',
+            //validate: idInput => { idInput ? true : console.log("Enter an id number"), false || typeof idInput === 'number' ? true : console.log("id must be a number"), false }
+        },
+        {
+            type: 'input',
+            message: "Enter email address:",
+            name: 'email',
+            //validate: emailInput => { emailInput ? true : console.log("Enter an email address"), false || emailInput === 'anystring@anystring.anystring' ? true : console.log("Enter a valid email address"), false }
+        },
+        {
+            type: 'input',
+            message: "Enter the engineer's GitHub username:",
+            name: 'username',
+            //validate: userNameInput => { userNameInput ? true : console.log("Enter a GitHub username"), false }
+        }
+    ])
+        .then(answers => {
+            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.username);
+            //push new engineer to team array
+            team.push(engineer);
+            choiceQuestion();
+        })
+};
+//inquirer questions for intern
+internQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: "Enter intern's name:",
+            name: 'name',
+            //validate: nameInput => { nameInput ? true : console.log("Enter a name"), false }
+        },
+        {
+            type: 'number',
+            message: "Enter intern's identification number:",
+            name: 'id',
+            //validate: idInput => { idInput ? true : console.log("Enter an id number"), false || typeof idInput === 'number' ? true : console.log("id must be a number"), false }
+        },
+        {
+            type: 'input',
+            message: "Enter intern's email address:",
+            name: 'email',
+            //validate: emailInput => { emailInput ? true : console.log("Enter an email address"), false || emailInput === 'anystring@anystring.anystring' ? true : console.log("Enter a valid email address"), false }
+        },
+        {
+            type: 'input',
+            message: "Enter the intern's school:",
+            name: 'school',
+            //validate: schoolInput => { schoolInput ? true : console.log("Enter a school name"), false }
+        },
+    ])
+        .then(answers => {
+            const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+            //push new intern to team array
+            team.push(intern);
+            choiceQuestion();
+        })
+};
 //Create a function to write html file
 function writeToFile(fileName) {
-    managerQuestions();
-    //push new manager to team array
-    team.push(manager);
-    //inquirer to create new team members
-    choiceQuestion();
     //function using file system to write html file
-    fs.writeFile(fileName, generateTeamPageHtml(), (err) =>
+    fs.writeFile(fileName, generateTeamPageHtml(team), (err) =>
         err ? console.error(err) : console.log('An HTML file has been created for your project!')
     )
 }
@@ -124,4 +229,5 @@ function init() {
     writeToFile('index.html');
 };
 //Function call to initialize app
-init();
+
+managerQuestions();
